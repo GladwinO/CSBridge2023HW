f3cfe67bd13133b27a71a70e79a746488313afaf
Gladwin <59264329+Gladwi@users.noreply.github.com>
2023-03-02 16:29:30 -0500

    Add files via upload

---
+++ gto219_hw8_q4.cpp
@@ -0,0 +1,20 @@
+// gto219_hw8_q4.cpp 
+#include <iostream>
+using namespace std;
+
+void randArrNumGen(int arr[]);
+
+int main()
+{
+    
+}
+
+void randArrNumGen(int arr[])
+{
+    int randNum;
+    int upperLim = 10, lowerLim;
+
+
+    randNum = (rand() % (upperLim - lowerLim + 1)) + lowerLim;
+}
+
