53fcdf1cb1a3bd76e78d3598afc9000438d98099
Gladwin <59264329+GladwinO@users.noreply.github.com>
2023-05-11 11:34:51 -0400

    Add files via upload

--- gto219_hw16.cpp
+++ gto219_hw16.cpp
@@ -38,7 +38,7 @@ public:
         return vecList.size();
     }
     void clear() {
-        while(!isEmpty()) {
+        while (!isEmpty()) {
             vecList.pop_back();
         }
     }
@@ -53,6 +53,10 @@ int main()
     std::string userInputLine;
     while (std::getline(userInputFile, userInputLine)) {
         std::string begin, pascChars, end;
+        char whiteSpacePos = userInputLine.find(' ');
+        while (whiteSpacePos != std::string::npos) {
+
+        }
 
     }
 
@@ -71,14 +75,3 @@ void openInputFile(std::ifstream& inFile) {
         inFile.open(fileName);
     }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
